87d8116806245b0aadf305a7a5a2f61e9dacb3d8
thiagosnx <thiagosantanacq@gmail.com>
2023-09-22 19:54:20 -0300

    add db.migrations e comentarios

--- src/main/java/com/podstore/controllers/ProductController.java
+++ src/main/java/com/podstore/controllers/ProductController.java
@@ -6,11 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+//bem- vindo, os controllers recebem as requisições dos usuários
+//apontamos o endpoint atraves do requestmapping
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/product") // endpoint
 public class ProductController {
-    @GetMapping
+    @GetMapping // criamos um metodo pro nosso controller
     public ResponseEntity getAllProducts(){
         return ResponseEntity.ok("deu ok");
     }
+    // o responseEntity é simplesmente uma resposta que volta para o usuario se der 200
 }
---
+++ src/main/java/com/podstore/domain/product/Product.java
@@ -0,0 +1,31 @@
+package com.podstore.domain.product;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+//bem-vindo, essa é a clase ENTIDADE,que representa a nossa tabela, mas nao dá pra manipular por aqui
+//para manipulá-la, usaremos os Repositorys
+
+//lembrando que criaremos os nossos codigos SQL pelo db.migrations
+//o FlyWay procura o arquivo dentro do db.migrations e executa os comandos sql que estao lá dentro
+@Table(name="product")
+@Entity(name="product") // entity representa o nosso banco de dados, precisamos criar as coisas aqui representando as colunas do db
+@Getter //getter, setter e contructors criados com lombok
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@EqualsAndHashCode(of = "id")  // identifica a PrimaryKey do nosso DB
+public class Product {
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID) // mais uma vez, o @Id representa a PK
+    // o generatedvalue fica responsavel por gerar um id aleatorio para nos com o UUID
+    private String id;
+
+    private String name;
+
+    private Number price_in_cents;
+
+
+
+}
